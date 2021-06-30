71beb0642703e4f33f78c41cc31c8b3a86cd92b3
Vlad Zusko <vladzvu11@gmail.com>
2021-06-30 19:10:33 +0300

    Added basic join room implementation

--- socket/routes/rooms.js
+++ socket/routes/rooms.js
@@ -15,8 +15,29 @@ const addRoom = (roomname) =>
     members: [],
   });
 
+const joinRoom = (username, roomname) => {
+  const roomIndex = rooms.findIndex((room) => room.name == roomname);
+  rooms[roomIndex].members.push({
+    name: username,
+    status: 0,
+  });
+};
+
+const leftRoom = (username, roomname) => {
+  const roomIndex = rooms.findIndex((room) => room.name == roomname);
+  const userInder = rooms[roomIndex].members.findIndex(
+    (user) => user.name == username,
+  );
+  rooms[roomIndex].members.splice(userInder, 1);
+};
+
+const getRoom = (roomname) => rooms.find((room) => room.name == roomname);
+
 export default (io) => {
   io.on('connection', (socket) => {
+    const username = socket.handshake.query.username;
+    let joinedRoomName;
+
     socket.emit('UPDATE_ROOMS', getAvailableRooms());
 
     socket.on('CREATE_ROOM', (roomname) => {
@@ -24,6 +45,37 @@ export default (io) => {
         socket.emit('ROOM_EXISTS');
       } else {
         addRoom(roomname);
+        joinRoom(username, roomname);
+        socket.join(roomname);
+        joinedRoomName = roomname;
+
+        socket.emit('JOINED_ROOM', getRoom(roomname));
+        io.emit('UPDATE_ROOMS', getAvailableRooms());
+      }
+    });
+
+    socket.on('JOIN_ROOM', (roomname) => {
+      joinRoom(username, roomname);
+      socket.join(roomname);
+      joinedRoomName = roomname;
+
+      socket.emit('JOINED_ROOM', getRoom(roomname));
+      io.to(joinedRoomName).emit('UPDATE_ROOM', getRoom(roomname));
+      io.emit('UPDATE_ROOMS', getAvailableRooms());
+    });
+
+    socket.on('LEFT_ROOM', (roomname) => {
+      leftRoom(username, joinedRoomName);
+
+      io.to(joinedRoomName).emit('UPDATE_ROOM', getRoom(joinedRoomName));
+      io.emit('UPDATE_ROOMS', getAvailableRooms());
+    });
+
+    socket.on('disconnect', () => {
+      if (joinedRoomName) {
+        leftRoom(username, joinedRoomName);
+
+        io.to(joinedRoomName).emit('UPDATE_ROOM', getRoom(joinedRoomName));
         io.emit('UPDATE_ROOMS', getAvailableRooms());
       }
     });
