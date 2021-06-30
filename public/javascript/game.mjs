f5514d5cf372a57fac0b27a01c7e3b8f720fb10d
Vlad Zusko <vladzvu11@gmail.com>
2021-06-30 17:14:02 +0300

    Added front-end rooms adding implementation

--- public/javascript/game.mjs
+++ public/javascript/game.mjs
@@ -1,13 +1,43 @@
+import { clearRooms, addRoom } from './helpers/dom/rooms.mjs';
+
 const username = sessionStorage.getItem('username');
 
 if (!username) {
   window.location.replace('/login');
 }
 
-const loginSocket = io('/login', { query: { username } });
+const credentials = {
+  query: {
+    username,
+  },
+};
+
+const loginSocket = io('/login', credentials);
 
 loginSocket.on('USER_EXISTS', () => {
   alert('User with such a name already exists');
   sessionStorage.removeItem('username');
   window.location.replace('/login');
+});
+
+const roomsSocket = io('/rooms', credentials);
+const addRoomButton = document.getElementById('add-room-button');
+
+const addRoomHandler = () => {
+  const roomName = prompt('Enter room name:');
+
+  roomsSocket.emit('CREATE_ROOM', roomName);
+};
+
+addRoomButton.addEventListener('click', addRoomHandler);
+
+roomsSocket.on('UPDATE_ROOMS', (rooms) => {
+  clearRooms();
+  for (const room of rooms) {
+    addRoom(room);
+  }
+});
+
+roomsSocket.on('ROOM_EXISTS', () => {
+  alert('Room with such a name exists');
 });
