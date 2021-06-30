5e3ce5686cccc3376956ac9a6281fb189bf216fe
Vlad Zusko <vladzvu11@gmail.com>
2021-06-30 21:28:03 +0300

    Added text GET

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,13 +1,19 @@
-import { Router } from "express";
-import path from "path";
-import { HTML_FILES_PATH } from "../config";
+import { Router } from 'express';
+import path from 'path';
+import { HTML_FILES_PATH } from '../config';
+import { texts } from '../data';
 
 const router = Router();
 
-router
-  .get("/", (req, res) => {
-    const page = path.join(HTML_FILES_PATH, "game.html");
-    res.sendFile(page);
+router.get('/', (req, res) => {
+  const page = path.join(HTML_FILES_PATH, 'game.html');
+  res.sendFile(page);
+});
+
+router.get('/texts/:id', (req, res) => {
+  res.json({
+    text: texts[req.params.id],
   });
+});
 
 export default router;
