93646145d223c8f0d780aa897d041a938b688c2e
Vlad Zusko <vladzvu11@gmail.com>
2021-07-02 01:32:52 +0300

    Added Heroku setup

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import express from "express";
-import http from "http";
-import socketIO from "socket.io";
-import socketHandler from "./socket";
-import routes from "./routes";
-import { STATIC_PATH, PORT } from "./config";
+import express from 'express';
+import http from 'http';
+import socketIO from 'socket.io';
+import socketHandler from './socket';
+import routes from './routes';
+import { STATIC_PATH, PORT } from './config';
 
 const app = express();
 const httpServer = http.Server(app);
@@ -12,8 +12,8 @@ const io = socketIO(httpServer);
 app.use(express.static(STATIC_PATH));
 routes(app);
 
-app.get("*", (req, res) => {
-  res.redirect("/login");
+app.get('*', (req, res) => {
+  res.redirect('/login');
 });
 
 socketHandler(io);
