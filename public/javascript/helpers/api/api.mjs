ed5ba37c3bbcc6e9c2ff1ab6bb97a3b30354ac28
Vlad Zusko <vladzvu11@gmail.com>
2021-07-03 18:32:50 +0300

    Added texts proxy for front-end

--- public/javascript/helpers/api/api.mjs
+++ public/javascript/helpers/api/api.mjs
@@ -1,8 +1,16 @@
 const textsRoute = `${window.location.href}/texts/`;
 
+const textsProxy = new Map();
+
 export async function getText(textId) {
+  if (textsProxy.has(textId)) {
+    return textsProxy.get(textId);
+  }
+
   const text = (
     await fetch(textsRoute + textId).then((response) => response.json())
   ).text;
+  textsProxy.set(textId, text);
+
   return text;
 }
