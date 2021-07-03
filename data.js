01511e8f823f2d04acd1cd811a97e5bb9dadbb18
Vlad Zusko <vladzvu11@gmail.com>
2021-07-03 11:43:10 +0300

    Added jokes

--- data.js
+++ data.js
@@ -8,4 +8,15 @@ export const texts = [
   'Text for typing #7',
 ];
 
-export default { texts };
+export const jokes = [
+  'A man got hit in the head with a can of Coke. Thank goodness it was a soft drink.',
+  'What do you call cheese that isn’t yours? Nacho Cheese.',
+  'My girlfriend and I often laugh about how competitive we are. But I laugh more.',
+  'What do you call a fish with no eye? Fssshh.',
+  'What’s white and can’t climb trees? A fridge.',
+  'A communist joke isn’t funny…unless everyone gets it.',
+  'What’s the stupidest animal in the jungle? A polar bear.',
+  'Don’t you hate it when someone answers their own questions? I do.',
+  'What do you call a fake noodle? An Impasta.',
+  'The shovel was a ground-breaking invention.',
+];
